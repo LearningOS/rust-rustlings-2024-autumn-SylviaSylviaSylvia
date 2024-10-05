5c8f23ca31e9204a9c2a558744151bafdee1cf9b
SylviaSylviaSylvia <13097633568@163.com>
2024-10-05 12:47:15 +0800

    update

--- exercises/iterators/iterators2.rs
+++ exercises/iterators/iterators2.rs
@@ -6,16 +6,15 @@
 // Execute `rustlings hint iterators2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 // Step 1.
 // Complete the `capitalize_first` function.
 // "hello" -> "Hello"
 pub fn capitalize_first(input: &str) -> String {
     let mut c = input.chars();
-    match c.next() {
+    match c.next(){
         None => String::new(),
-        Some(first) => ???,
+        Some(first) => first.to_uppercase().collect::<String>() + c.as_str()
     }
 }
 
@@ -24,7 +23,15 @@ pub fn capitalize_first(input: &str) -> String {
 // Return a vector of strings.
 // ["hello", "world"] -> ["Hello", "World"]
 pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
-    vec![]
+    let mut v = vec![];
+    let mut words_iter = words.iter();
+    loop{
+        match words_iter.next(){
+            Some(val) => v.push(capitalize_first(val)),
+            None => break,
+        }
+    }
+    v
 }
 
 // Step 3.
@@ -32,7 +39,15 @@ pub fn capitalize_words_vector(words: &[&str]) -> Vec<String> {
 // Return a single string.
 // ["hello", " ", "world"] -> "Hello World"
 pub fn capitalize_words_string(words: &[&str]) -> String {
-    String::new()
+    let mut s = String::new();
+    let mut words_iter = words.iter();
+    loop{
+        match words_iter.next(){
+            Some(val) => s += &capitalize_first(val),
+            None => break,
+        }
+    }
+    s
 }
 
 #[cfg(test)]
--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,7 +3,6 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
     // Complete this function to return the factorial of num
@@ -14,8 +13,12 @@ pub fn factorial(num: u64) -> u64 {
     // - additional variables
     // For an extra challenge, don't use:
     // - recursion
-    // Execute `rustlings hint iterators4` for hints.
+    // Execute `rustlings hint iterators4` for hints
+    (1..=num).rfold(1, |acc, x| acc * x)
 }
+//第一个参数 1 是初始累加器值，表示阶乘计算的起始值。
+//第二个参数是一个闭包，定义了如何组合累加器 acc 和当前迭代项 x。
+//在这个闭包中，acc 是累积的中间结果，x 是当前迭代的值。闭包的返回值 acc * x 用于更新累加器。
 
 #[cfg(test)]
 mod tests {
